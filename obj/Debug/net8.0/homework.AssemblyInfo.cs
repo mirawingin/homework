3484ea25d3438e036f2a43987305aeec414ac4e0
mirawingin <mirawingin174@gmail.com>
2024-03-15 14:29:04 +0500

    Added a block diagram

--- obj/Debug/net8.0/homework.AssemblyInfo.cs
+++ obj/Debug/net8.0/homework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3ee6cd72d0d16bcc278905ac8d964204dc5756")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e42ff27d7b8d7dc59739dead4021ba5c46bcf18")]
 [assembly: System.Reflection.AssemblyProductAttribute("homework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
